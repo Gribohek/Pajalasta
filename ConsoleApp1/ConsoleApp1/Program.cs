44a9fbc45bc39c15d5a2f1e08984c4681a74e2bf
andrew09090870 <andrew09090870@yandex.ru>
2024-05-21 17:59:12 +0700

    КОРЕСПОДЕНТУ ПИВО ОТ ГЕНЕРАЛА АЯНА

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -34,13 +34,10 @@ namespace ConsoleApp1
                     KB.Add(new KeyboardButton("Гойда"));
                     KB.Add(new KeyboardButton("Пиво пить :D"));
                 }
-                {
-                    ResizeKeyboard = true,
-                };
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Добро пожаловать в игру Лабиринт проклятых душ, для начала игры напишите гойда", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
             }
-            if (message.Text.ToLower().Contains("гойда"))
+            else if (message.Text.ToLower().Contains("гойда"))
             {
                 {
                     KB.Add(new KeyboardButton("ЗА ПУТИНА!!!!!!!!!!!!!!!!"));
@@ -49,17 +46,10 @@ namespace ConsoleApp1
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Ты просыпаешся в темном и запутанном лабиринте, ощущая сильное беспокойство и страх. У тебя нет никаких воспоминаний о том, кто ты такой и как зддесь оказался. Вокруг тебя стены лабиринта, выглядящие совершенно непроницаемыми, но на них видны странные символы и вырезанные пазлы.", replyMarkup: new ReplyKeyboardMarkup(KB));
 
             }
-            if (message.Text.ToLower().Contains("осмотреться в комнате"))
+            else
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы решаете начать исследование комнаты и находите первый пазл. Вы осматриваете его и с помощью наблюдательности и сообразительности решает его. Таинственная дверь, посреди одной из стен, открывается, и игрок входит в следующую комнату.");
-            }
-            if (message.Text.ToLower().Contains("войти в открывшуюся дверь"))
-            {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы входите в следующую комнату и там сталкиваетесь с несколькими чудовищными существами, которые незамедлительно нападают на вас. Но вместо того чтобы убить вас, они объясняют, что они испытывают его силу и интеллект, потому что они также являются проклятыми душами, попавшими в этот лабиринт.");
-            }
-            if (message.Text.ToLower().Contains("напасть на монстра"))
-            {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Путешественник бросается в схватку с первым монстром. Битва проходит в формате боя на выживание, где путешественнику приходится использовать свое мастерство и стратегию, чтобы победить. После этого победы игрок получает уважение монстров и продолжает свой путь по лабиринту.");
+                await botClient.SendTextMessageAsync(message.Chat.Id,"Ебанько?", replyMarkup: new ReplyKeyboardMarkup(KB));
+
             }
 
         }
