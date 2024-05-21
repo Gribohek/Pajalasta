efeb26e2d5750a4dc7cce9a261b6c0c68301eb22
pozdnyakov <antonp1602@gmail.com>
2024-05-21 19:23:29 +0700

    я кончил

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -35,99 +35,88 @@ namespace ConsoleApp1
                 }
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Добро пожаловать в мою замечательную игру! Ты находишься в загадочном особняке, где тебе предстоит пройти через несколько комнат и разгадать головоломки. Готов начать свое приключение?", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("конечно")||(message.Text.ToLower().Contains("да")))
+            else if (message.Text.ToLower().Contains("конечно")|| message.Text.ToLower().Contains("да"))
             {
                 {
                     KB.Add(new KeyboardButton("В первую"));
                 }
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Перед тобой пять комнат. Выбери, в какую ты хочешь пойти первой.", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("в первую"))
+         
+            else if (message.Text.ToLower().Contains("в первую") || message.Text.ToLower().Contains("первую") || message.Text.ToLower().Contains("1") || message.Text.ToLower().Contains("в первую комнату"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
                 KB.Add(new KeyboardButton("Ключ"));
+                KB.Add(new KeyboardButton("Свечу"));
                 KB.Add(new KeyboardButton("Меч"));
+                KB.Add(new KeyboardButton("Зеркало"));
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Перед тобой стол с пятью предметами: книга, ключ, свеча, зеркало, меч. Какой предмет ты выберешь?", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            else
-
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Dнутреннее чувство подсказывает тебе идти в первую комнату", replyMarkup: new ReplyKeyboardMarkup(KB));
-                return;
-
-
-            if (message.Text.ToLower().Contains("свечу"))
+            else if (message.Text.ToLower().Contains("книгу") || message.Text.ToLower().Contains("я выбираю книгу") || message.Text.ToLower().Contains("книга"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
                 KB.Add(new KeyboardButton("Ключ"));
+                KB.Add(new KeyboardButton("Свечу"));
                 KB.Add(new KeyboardButton("Меч"));
-                await botClient.SendTextMessageAsync(message.Chat.Id, "персонаж подходит и зажигает свечу, но ничего не происходит и свеча тут же тухнет", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Add(new KeyboardButton("Зеркало"));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы осматриваете старую покрытую пылью книгу и решаете её не брать с собой за ненадобностью", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("зеркало"))
+            else if (message.Text.ToLower().Contains("свечу") || message.Text.ToLower().Contains("я выбираю свечу") || message.Text.ToLower().Contains("свеча"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
                 KB.Add(new KeyboardButton("Ключ"));
+                KB.Add(new KeyboardButton("Свечу"));
                 KB.Add(new KeyboardButton("Меч"));
-                await botClient.SendTextMessageAsync(message.Chat.Id, "персонаж подбирает зеркало, осматривает себя в нём и кладёт обратно", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Add(new KeyboardButton("Зеркало"));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы подходите и зажигаете свечу, но ничего не происходит и свеча тут же тухнет", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("меч"))
+            else if (message.Text.ToLower().Contains("зеркало") || message.Text.ToLower().Contains("я выбираю зеркало"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
                 KB.Add(new KeyboardButton("Ключ"));
+                KB.Add(new KeyboardButton("Свечу"));
                 KB.Add(new KeyboardButton("Меч"));
+                KB.Add(new KeyboardButton("Зеркало"));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы подбираете зеркало, осматриваете себя в нём и кладёт обратно", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Clear();
+            }
+            else if (message.Text.ToLower().Contains("меч") || message.Text.ToLower().Contains("я выбираю меч"))
+            {
+                KB.Add(new KeyboardButton("Ключ"));
+                KB.Add(new KeyboardButton("Свечу"));
+                KB.Add(new KeyboardButton("Меч"));
+                KB.Add(new KeyboardButton("Зеркало"));
                 await botClient.SendTextMessageAsync(message.Chat.Id, "персонаж решает забрать меч с собой", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("ключ"))
+            else if (message.Text.ToLower().Contains("ключ") || message.Text.ToLower().Contains("я выбираю ключ"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
-                KB.Add(new KeyboardButton("Ключ"));
-                KB.Add(new KeyboardButton("Меч"));
+                KB.Add(new KeyboardButton("Перейти в следующую комнату"));
                 await botClient.SendTextMessageAsync(message.Chat.Id, "Ключ открывает спрятанную в стене дверь, ведущую в следующую комнату. Переходи туда и продолжай свое путешествие.", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("остаться в комнате"))
+            else if (message.Text.ToLower().Contains("перейти в следующую комнату") || message.Text.ToLower().Contains("иди в следующую комнату") || message.Text.ToLower().Contains("пойти в следующую комнату"))
             {
-                KB.Add(new KeyboardButton("Свечу"));
-                KB.Add(new KeyboardButton("Зеркало"));
-                KB.Add(new KeyboardButton("Ключ"));
-                KB.Add(new KeyboardButton("Меч"));
-                await botClient.SendTextMessageAsync(message.Chat.Id, "В ходе осмотра он находит письмо в котором сказано, что нужно взять зеркало с собой", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Add(new KeyboardButton("Посмотреть вверх"));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Ты переходишь в следующую комнату, где находит записку", replyMarkup: new ReplyKeyboardMarkup(KB));
                 KB.Clear();
-                return;
             }
-            if (message.Text.ToLower().Contains("перейти в следующую комнату"))
+            else if (message.Text.ToLower().Contains("посмотреть вверх") || message.Text.ToLower().Contains("посмотри наверх"))
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Пользователь переходит в следующую комнату, где находит записку. Записка: Чтобы разгадать следующую головоломку, найди ключ к соединению небесных тел.", replyMarkup: new ReplyKeyboardMarkup(KB));
-            }
-
-            if (message.Text.ToLower().Contains("посмотреть вверх"))
-            {
+                KB.Add(new KeyboardButton("Зайти в следующую комнату"));
                 await botClient.SendTextMessageAsync(message.Chat.Id, " Вы смотрите наверх, и ты находите спрятанный на запылившейся полке ключ. Открой следующую дверь и попадете в конечную комнату.", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Clear();
             }
             else if (message.Text.ToLower().Contains("зайти в следующую комнату"))
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Пользователь заходит в конечную комнату и встречает босса. Ты смело преодолел все преграды, но перед тобой стою я - босс этого замка. Готов ли ты сразиться со мной?", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Add(new KeyboardButton("Я готов"));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Вы заходите в конечную комнату и встречает босса. Ты смело преодолел все преграды, но перед тобой стою я - босс этого замка. Готов ли ты сразиться со мной?", replyMarkup: new ReplyKeyboardMarkup(KB));
+                KB.Clear();
             }
-            else if (message.Text.ToLower().Contains("я готов!"))
+            else if (message.Text.ToLower().Contains("я готов"))
             {
-                await botClient.SendTextMessageAsync(message.Chat.Id, "Начинается битва между пользователем и боссом. Пользователь использует навыки, которые накопил за время путешествия, и наконец побеждает босса. поздравляю вы прошли игру.", replyMarkup: new ReplyKeyboardMarkup(KB));
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Начинается битва между пользователем и боссом. Пользователь использует навыки, которые накопил за время путешествия, и наконец побеждает босса. Поздравляю вы прошли игру.", replyMarkup: new ReplyKeyboardMarkup(KB));
             }
             else 
             {
